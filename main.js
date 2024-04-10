2cdf58d94dc22af980acf944514436e0872bc615
RuadhanKM <9281239@students.k12.dc.us>
2024-04-10 16:32:42 -0400

    kernel calculation

--- main.js
+++ main.js
@@ -4,12 +4,16 @@ const ctx = canvas.getContext("2d");
 const mainGrid = new Float32Array(canvas.width * canvas.height);
 const imageData = new ImageData(canvas.width, canvas.height);
 
+function getFlattenedWrappedPixelOffset(x, y) {
+    return (((y % canvas.height) + canvas.height) % canvas.height) * canvas.width + (((x % canvas.width) + canvas.width) % canvas.width);
+}
+
 function getGrid(x, y) {
-    return mainGrid[(((y % canvas.height) + canvas.height) % canvas.height) * canvas.width + (((x % canvas.width) + canvas.width) % canvas.width)];
+    return mainGrid[getFlattenedWrappedPixelOffset(x, y)];
 }
 
 function setGrid(x, y, v) {
-    let o = (((y % canvas.height) + canvas.height) % canvas.height) * canvas.width + (((x % canvas.width) + canvas.width) % canvas.width);
+    let o = getFlattenedWrappedPixelOffset(x, y);
     mainGrid[o] = v;
     imageData.data[o * 4 + 0] = v*255;
     imageData.data[o * 4 + 1] = v*255;
@@ -17,10 +21,13 @@ function setGrid(x, y, v) {
     imageData.data[o * 4 + 3] = 255;
 }
 
-const kernelSize = 1;
+const kernelSize = 4;
+const updateFrequency = 50;
+let showKernel = true;
 
 function calculateKernelWeight(ox, oy) {
-    return 1;
+    let dis = Math.sqrt(ox**2+oy**2);
+    return Math.exp(-((dis-kernelSize/2)**2)/kernelSize);
 }
 
 function getPixelKernel(x, y) {
@@ -55,15 +62,34 @@ ctx.putImageData(imageData, 0, 0);
 function renderLoop() {
     for (let y=0; y<canvas.height; y++) {
         for (let x=0; x<canvas.width; x++) {
-            setGrid(x, y, getGrid(x, y) + growth(getPixelKernel(x, y)));
+            setGrid(x, y, getGrid(x, y) + growth(getPixelKernel(x, y)/updateFrequency ));
         }
     }
-
-
+    
+    if (showKernel) {
+        // Display kernel
+        for (let y=0; y<kernelSize*2+1; y++) {
+            for (let x=0; x<kernelSize*2+1; x++) {
+                let o = getFlattenedWrappedPixelOffset(x, y);
+                let val = calculateKernelWeight(x - kernelSize, y - kernelSize);
+                imageData.data[o * 4 + 0] = (1-val)*255;
+                imageData.data[o * 4 + 1] = val*255;
+                imageData.data[o * 4 + 2] = 0;
+                imageData.data[o * 4 + 3] = 255;
+            }
+        }
+    }
 
     ctx.putImageData(imageData, 0, 0);
 }
 
+setInterval(renderLoop, 0);
 
+{
+    let kernelButton = document.getElementById("toggleKernel");
 
-setInterval(renderLoop, 500);
+    kernelButton.addEventListener("click", () => {
+        showKernel = !showKernel;
+        kernelButton.innerText = showKernel ? "Hide Kernel" : "Show Kernel";
+    });
+}
